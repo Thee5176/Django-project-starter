65c52ebfdf60f867435b6c0efc8082328fae4dff
Thee5176 <Theerapong5176@gmail.com>
2024-12-15 09:49:09 +0900

    Config Staticfiles

---
+++ README.md
@@ -0,0 +1,11 @@
+## Pre-set up checklist
+- [x] Custom User Model
+- [x] UserAuthentication 1# (Login/Logout/Signup)
+- [x] Config Staticfiles
+- [ ] Bootstrap CDN
+- [ ] UserAuthentication 2# (Password)
+- [ ] Config Email
+- [ ] Deployment setting (debug,host,secret key)
+- [ ] Database
+
+
--- django_project/settings.py
+++ django_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
 ]
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']    # development directory
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
